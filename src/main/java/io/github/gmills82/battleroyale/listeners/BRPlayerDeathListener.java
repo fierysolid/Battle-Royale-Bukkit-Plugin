178757717d8a86321e59ab8acd1392741d22f96d
Grant Mills <grant.mills@bodybuilding.com>
2016-05-20 16:31:16 -0400

    basic death listener to remove players from plugin battle players list

---
+++ src/main/java/io/github/gmills82/battleroyale/listeners/BRPlayerDeathListener.java
@@ -0,0 +1,37 @@
+package io.github.gmills82.battleroyale.listeners;
+
+import io.github.gmills82.battleroyale.BattleRoyalePlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.List;
+
+/**
+ * @author Grant Mills
+ * @since 5/20/16
+ */
+public class BRPlayerDeathListener implements Listener {
+
+	private final BattleRoyalePlugin plugin;
+
+	public BRPlayerDeathListener(BattleRoyalePlugin plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event) {
+		Player playerLoggingOn = event.getEntity();
+
+		for(Player player : this.plugin.getCurrentBattlePlayers()) {
+			if(player.getName().equalsIgnoreCase(playerLoggingOn.getName())) {
+
+				//Remove player and reset list
+				List<Player> newPlayerList = this.plugin.getCurrentBattlePlayers();
+				newPlayerList.remove(player);
+				this.plugin.setCurrentBattlePlayers(newPlayerList);
+			}
+		}
+	}
+}
